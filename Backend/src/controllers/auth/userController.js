d70f406b3b2be993bd7c25c0209181f478957c72
Mrinank Gaur <mrinank2484@gmail.com>
2025-03-03 21:30:23 +0530

    feat: user data fetch

--- Backend/src/controllers/auth/userController.js
+++ Backend/src/controllers/auth/userController.js
@@ -129,4 +129,20 @@ export const loginUser = asyncHandler(async (req,res)=>{
 export const logoutUser = asyncHandler(async (req,res)=>{
     res.clearCookie("token");
     res.status(200).json({"message":"User Logged Out"});
-})
+});
+
+//get user profile;
+
+export const getUser = asyncHandler(async (req,res)=>{
+    //get user details from the token --> exclude password
+    const user = await User.findById(req.user._id).select("-password");
+
+    if(user){
+        res.status(200).json(user);
+    }
+    else{
+        //404 Not Found
+        res.status(404).json({"message":"User Not Found"});
+    }
+
+});
--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth/userController.js";
+import { getUser, loginUser, logoutUser, registerUser } from "../controllers/auth/userController.js";
+import { protect } from "../middleware/authMiddleWare.js";
 
 const router = express.Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+router.get("/logout",logoutUser);
+router.get("/user",protect,getUser);
 
 export default router;
