5233cb03a8d48f7bc51cdb53d8647d488c5e7b31
Mrinank Gaur <mrinank2484@gmail.com>
2025-03-04 18:14:06 +0530

    feat: update user

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, loginUser, logoutUser, registerUser } from "../controllers/auth/userController.js";
+import { getUser, loginUser, logoutUser, registerUser, updateUser } from "../controllers/auth/userController.js";
 import { protect } from "../middleware/authMiddleWare.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/register",registerUser);
 router.post("/login",loginUser);
 router.get("/logout",logoutUser);
 router.get("/user",protect,getUser);
+router.patch("/user",protect,updateUser);
 
 export default router;
